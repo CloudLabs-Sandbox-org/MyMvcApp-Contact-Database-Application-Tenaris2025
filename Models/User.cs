ec6ff6c11d104190798a852e2b466fe341fc2eb1
github_cloudlabsuser_2142 <github_cloudlabsuser_2142@cloudlabsaiuser.com>
2025-03-26 15:26:56 +0000

    Search bar

--- Models/User.cs
+++ Models/User.cs
@@ -12,5 +12,16 @@ namespace MyMvcApp.Models
         public string Name { get; set; }
         public string Email { get; set; }
 
+        public User(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+
+    public User()
+    {
+
+    }
     }
 }
